dbe74f903b30535db26b04f9dcea1f37f8e63952
Seamus Bradley <github@scmb.xyz>
2022-02-09 16:31:37 +0000

    Add history and points columns

--- basic.py
+++ basic.py
@@ -2,11 +2,16 @@ from pathlib import Path
 import requests
 import pandas
 
+
+# Download data if it doesn't exist, or if forced
+
 data_url = "https://raw.githubusercontent.com/jalapic/engsoccerdata/master/data-raw/england.csv"
 
 data_raw = Path("england.csv")
 
-if not data_raw.is_file():
+force = False
+
+if force or not data_raw.is_file():
     download = requests.get(data_url)
     data_raw.write_text(download.content.decode("utf-8"))
 
@@ -20,17 +25,7 @@ data_f = data[(data["Season"] >= 1995) & (data["tier"] == 1)]
 df = data_f[["Date", "Season", "home", "visitor", "result"]].copy()
 
 
-results_dict = {}
-
-for team in df["home"].unique():
-    results_dict[team] = ""
-
-
-def last_five(result, previous):
-    if len(previous) < 5:
-        return previous + result
-    else:
-        return previous[-4:] + result
+# Helper functions for extracting a team's games for a season
 
 
 def games_with_team(name, frame=df):
@@ -50,6 +45,8 @@ def get_history_frame(team, season, before=None, frame=df):
     return in_season
 
 
+# Add columns for a teams results so far
+
 home_result = {"H": "W", "D": "D", "A": "L"}
 away_result = {"H": "L", "D": "D", "A": "W"}
 
@@ -64,33 +61,56 @@ def game_to_result(game, team):
 def get_history_string(team, season, before=None, frame=df):
     hist_frame = get_history_frame(team, season, before, frame)
     result_series = hist_frame.apply(lambda x: game_to_result(x, team), axis=1)
-    return "".join(result_series.tolist())
+    return "".join(result_series.values.tolist())
 
 
-# def make_history(team_name, result):
-#     previous_results = results_dict[team_name]
-#     updated_results = last_five(result, previous_results)
-#     results_dict[team_name] = updated_results
-#     return previous_results
-# pf_away = []
-# pf_home = []
-# # This is slow. Is there a better way?
-# for row in df.iterrows():
-#     r = row[1]
-#     home_name = r["home"]
-#     away_name = r["visitor"]
-#     h_result = home_result[r["result"]]
-#     a_result = away_result[r["result"]]
-#     pf_home.append(make_history(home_name, h_result))
-#     pf_away.append(make_history(away_name, a_result))
+print("home history")
+df["home_history"] = df.apply(
+    lambda x: get_history_string(x["home"], x["Season"], before=x["Date"]), axis=1
+)
 
-# pf_home_3 = [x[-3:] for x in pf_home]
-# pf_away_3 = [x[-3:] for x in pf_away]
 
-# df["past_five_home"] = pf_home
-# df["past_five_away"] = pf_away
-# df["past_three_home"] = pf_home_3
-# df["past_three_away"] = pf_away_3
+print("away history")
+df["away_history"] = df.apply(
+    lambda x: get_history_string(x["visitor"], x["Season"], before=x["Date"]), axis=1
+)
+
+
+points_dict = {"W": 3, "D": 1, "L": 0}
+
+
+def points_from_history(s):
+    return sum([points_dict[x] for x in s])
+
+
+print("home_points")
+df["home_points"] = df.apply(lambda x: points_from_history(x["home_history"]), axis=1)
+
+print("away points")
+df["away_points"] = df.apply(lambda x: points_from_history(x["away_history"]), axis=1)
+
+# It will be useful later to extract the last five games, for example, from the full series
+def last_n_games(frame, n, away=False):
+    if away:
+        column = "away_history"
+    else:
+        column = "home_history"
+    return frame[column].values.tolist()[-n]
+
+
+# Also useful, as a comparison, the final record of results and points for a season
+
+
+def get_full_season(team, season):
+    hf = get_history_frame(team, season)
+    return hf.values.tolist()[-1]["home_history"]
+
+
+def get_final_points(team, season):
+    return points_from_history(get_full_season(team, season))
+
 
 clean_out = Path("england_clean.csv")
 df.to_csv(clean_out)
+
+print("Done")
