138081262f561233daacae207890c562fb1ded9b
Seamus Bradley <github@scmb.xyz>
2022-04-09 17:04:01 +0100

    Add docstrings and comments to basic.py

--- basic.py
+++ basic.py
@@ -19,17 +19,23 @@ if force or not data_raw.is_file():
 with open(data_raw) as d:
     data = pandas.read_csv(d)
 
-# Focus on 20 team premier league : season >= 1995, tier= 1
+# To focus on 20 team premier league : season >= 1995, tier= 1
+# uncomment the next line, and comment out the one after.
 
 # data_f = data[(data["Season"] >= 1982) & (data["tier"] <= 2)]
 
 data_f = data
 
+# Remove a lot of extraneous columns.
 df = data_f[["Date", "Season", "tier", "home", "visitor", "result"]].copy()
 
+
+# We now add columns for the season-to-date win/loss history.
+# This is quite slow, because it's using apply.
+# We should figure out how to do the same with groupby and cumsum.
 start = time.monotonic()
 df_groups = df.groupby("Season")
-print("history")
+print("Generating history")
 history_groups = []
 for year, season in df_groups:
     print(year)
@@ -50,24 +56,8 @@ for year, season in df_groups:
 df["home_history"] = pandas.concat(history_groups)["home_history"]
 df["away_history"] = pandas.concat(history_groups)["away_history"]
 
-# df["home_history"] = df.apply(
-#     lambda x: utilities.get_history_string(
-#         x["home"], x["Season"], df, before=x["Date"]
-#     ),
-#     axis=1,
-# )
 
-
-# print("away history")
-# df["away_history"] = df.apply(
-#     lambda x: utilities.get_history_string(
-#         x["visitor"], x["Season"], df, before=x["Date"]
-#     ),
-#     axis=1,
-# )
-
-
-print("points")
+print("Generating points")
 df["home_points"] = df.apply(
     lambda x: utilities.points_from_history(x["home_history"]), axis=1
 )
@@ -75,7 +65,7 @@ df["away_points"] = df.apply(
     lambda x: utilities.points_from_history(x["away_history"]), axis=1
 )
 
-
+# Write the useful information to a new file.
 clean_out = Path("england_clean.csv")
 df.to_csv(clean_out)
 
