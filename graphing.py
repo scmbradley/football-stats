60f499f57035e5442ee9803bb6183a70d268917c
Seamus Bradley <github@scmb.xyz>
2022-04-17 10:18:46 +0100

    Use Seaborn for style.

--- graphing.py
+++ graphing.py
@@ -9,6 +9,13 @@ score_frame_path = Path("score_frame.csv")
 with open(score_frame_path) as d:
     sl = pd.read_csv(d)
 sl.set_index("type", inplace=True)
-sl.sort_values("log_norm")[["log_norm", "brier_norm"]].plot.barh()
 
-plt.show()
+sns.set_theme()
+sns.set_style("white")
+
+
+sl.sort_values("log_norm")[["log_norm", "brier_norm"]].plot.barh()
+# plt.xticks(rotation=45)
+plt.legend(loc="lower right")
+sns.despine(left=True)
+plt.savefig("summary.png")
